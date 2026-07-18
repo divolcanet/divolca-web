dd2f7b38104bcd7412c14cfb5c3e5bc04b6cc546
anjarhariadi <anjarmath@gmail.com>
2026-07-18 16:16:52 +0700

    fix: remove unused scene

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -1,33 +1,15 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "../components/NavBar";
 import Footer from "../components/Footer";
 import NoupeChatbot from "../components/NoupeChatbot";
 import VolcanoEventStats from "../components/VolcanoEventStats";
-import { Scene3DCanvas } from "../components/Scene3DCanvas";
 import { useScroll } from "../hooks/useScroll";
 import { cn } from "../lib/utils";
 
-const SCENE_PAGES = ["/", "/full"];
-
 export default function AppLayout() {
   const scrolled = useScroll(50);
-  const { pathname } = useLocation();
-  const showScene = SCENE_PAGES.includes(pathname);
-
   return (
     <div className="min-h-svh flex flex-col">
-      <div
-        className={cn(
-          "fixed inset-0 z-0 transition-opacity duration-500",
-          showScene
-            ? "opacity-100 pointer-events-auto"
-            : "opacity-0 pointer-events-none",
-        )}
-        aria-hidden={!showScene}
-      >
-        <Scene3DCanvas />
-      </div>
-
       <div className="relative z-10 flex flex-col min-h-svh">
         <div
           className={cn(
